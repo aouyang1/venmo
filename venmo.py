193a572edd16f6bf37410918f19e1c437650f60e
Austin Ouyang <aouyang1@gmail.com>
2016-08-21 01:48:48 -0700

    revised to continually collect each EOD utc and grab data for a specified day. Also uploads to s3 automatically

--- venmo.py
+++ venmo.py
@@ -2,34 +2,45 @@
 
 import sys
 import os
+import re
 import requests
 import json
-from datetime import datetime
+from time import sleep
+from datetime import datetime, timedelta
 from pprint import pprint
+
+import boto3
 
 sess = requests.Session()
 adapter = requests.adapters.HTTPAdapter(max_retries=5)
 sess.mount('http://', adapter)
 sess.mount('https://', adapter)
 
-def collect_data_between(limit=20, page="https://venmo.com/api/v5/public?", folder="/data", start_dt="2016_08_01", end_dt="2016_08_01"):
-    if not os.path.exists(folder):
-	os.makedirs(folder)
+def collect_data_between(limit=20, page="https://venmo.com/api/v5/public?", folder="data", start_dstr="2016_08_01", end_dstr="2016_08_01"):
 
-    fname = "venmo_{}.json".format(start_dt)
+    try:
+        start_dt = datetime.strptime(start_dstr,"%Y_%m_%d")
+        end_dt = datetime.strptime(end_dstr, "%Y_%m_%d")
+    except:
+        print("Cannot parse start and/or end date")
+        raise
+
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    fname = "venmo_{}.json".format(start_dstr)
     f_venmo = open(folder+"/"+fname, "w")
 
-    start_year, start_month, start_day = map(lambda x: int(x), start_dt.split("_"))
-    end_year, end_month, end_day = map(lambda x: int(x), end_dt.split("_"))
-
     epoch_dt = datetime(1970,1,1)
-    start_ts = int((datetime(start_year, start_month, start_day) - epoch_dt).total_seconds())
-    end_ts = int((datetime(end_year, end_month, end_day) - epoch_dt).total_seconds())
+    start_ts = int((start_dt - epoch_dt).total_seconds())
+    end_ts = int((end_dt - epoch_dt).total_seconds())
 
     page += "until={}".format(end_ts)
 
     while True:
         page = page.replace(":8100","")
+        print(page)
         r = sess.get(page + "&limit={}".format(limit)).json()
 
         if len(r["data"]) == 0:
@@ -42,14 +53,44 @@ def collect_data_between(limit=20, page="https://venmo.com/api/v5/public?", fold
             ts = (dt - epoch_dt).total_seconds()
 
             if ts < start_ts:
-                return
+                return f_venmo
             else:
                 f_venmo.write("{}\n".format(json.dumps(item)))
 
+def daily_collect():
+    prev_dt = datetime.utcnow()
+    while True:
+        curr_dt = datetime.utcnow()
+        if curr_dt.date() != prev_dt.date():
+            start_dstr = (curr_dt.date() - timedelta(days=1)).strftime("%Y_%m_%d")
+            collect_day(start_dstr)
+
+        prev_dt = curr_dt
+        sleep(60)
+
+def collect_day(start_dstr):
+    start_dt = datetime.strptime(start_dstr, "%Y_%m_%d")
+    end_dstr = (start_dt + timedelta(days=1)).strftime("%Y_%m_%d")
+    f_venmo = collect_data_between(limit=1200, start_dstr=start_dstr, end_dstr=end_dstr)
+    push_to_s3(f_venmo)
+
+def push_to_s3(f_venmo):
+    print("uploading to s3...")
+    filename_path = f_venmo.name
+    filename = filename_path.split("/")[-1]
+    year, month, day = re.search("(?<=venmo_)\w+", filename).group(0).split("_")
+    
+    s3_client = boto3.client('s3', aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"], aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"])
+    s3_client.upload_file(filename_path, "venmo-data-monthly", year+"_"+month+"/"+filename)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    start_dt = args[0]
-    end_dt = args[1]
-    folder = args[2]
-    collect_data_between(limit=1200, folder=folder, start_dt=start_dt, end_dt=end_dt)
+    option = args[0]
+
+    if option == "run":
+        daily_collect()
+    else:
+        start_dstr = option
+        collect_day(start_dstr)
+
+
