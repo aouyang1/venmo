f4a51cf7f5303d8985dd9b4e615387d30276f8b1
aouyang1 <aouyang1@gmail.com>
2017-04-25 11:37:41 -0700

    added simple spark setup for spot instances using pegasus

--- run_venmo_monster.sh
+++ run_venmo_monster.sh
@@ -2,6 +2,8 @@
 
 docker run -it \
   -v $(pwd):/root/venmo-monster \
+  -v /var/log/venmo-monster:/var/log/venmo-monster \
+  -e LOG_DIR=/var/log/venmo-monster \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e S3_BUCKET=$VENMO_S3_BUCKET \
